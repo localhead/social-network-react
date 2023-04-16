d11d516255625247c8e7ce93f036e0af7c0bc37b
localhead <dodoalive1994@gmail.com>
2023-04-16 18:28:51 +0600

    BrowserRouter changed to hashrouter

--- src/index.jsx
+++ src/index.jsx
@@ -4,16 +4,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   //<React.StrictMode>
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>
+  <>
+    <HashRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </HashRouter>
+  </>
+
   //</React.StrictMode>
 );
